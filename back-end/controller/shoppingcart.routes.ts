418e5d3baf8954d1404bf30e92885d0f67d9225e
MilanVermeiren <r0932151@ucll.be>
2024-12-20 13:57:54 +0100

    add Swagger documentation needs testing

--- back-end/controller/shoppingcart.routes.ts
+++ back-end/controller/shoppingcart.routes.ts
@@ -1,99 +1,323 @@
-import express, { NextFunction, Request, Response } from 'express';
-import shoppingcartService from '../service/shoppingcart.service';
+    import express, { NextFunction, Request, Response } from 'express';
+    import shoppingcartService from '../service/shoppingcart.service';
 
-const shoppingcartRouter = express.Router();
+    const shoppingcartRouter = express.Router();
 
-shoppingcartRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const shoppingcart = await shoppingcartService.getShoppingCartById({
-            id: parseInt(req.params.id),
-        });
-        res.status(200).json(shoppingcart);
-    } catch (error) {
-        next(error);
-    }
-});
-
-shoppingcartRouter.get('/user/:userId', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const shoppingcart = await shoppingcartService.getShoppingCartByUserId({
-            userId: parseInt(req.params.userId),
-        });
-        res.status(200).json(shoppingcart);
-    } catch (error) {
-        next(error);
-    }
-});
-
-shoppingcartRouter.get(
-    '/username/:username',
-    async (req: Request, res: Response, next: NextFunction) => {
+    /**
+     * @swagger
+     * info:
+     *   description: "Shopping Cart management API"
+     *   version: "1.0.0"
+     *   title: "Shopping Cart API"
+     * paths:
+     *   /shoppingcart/{id}:
+     *     get:
+     *       summary: "Get shopping cart by ID"
+     *       description: "Fetch a shopping cart by its unique ID."
+     *       operationId: "getShoppingCartById"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "id"
+     *           in: "path"
+     *           description: "ID of the shopping cart"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *       responses:
+     *         200:
+     *           description: "Shopping cart fetched successfully"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart not found"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/user/{userId}:
+     *     get:
+     *       summary: "Get shopping cart by user ID"
+     *       description: "Fetch a shopping cart by user ID."
+     *       operationId: "getShoppingCartByUserId"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "userId"
+     *           in: "path"
+     *           description: "ID of the user"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *       responses:
+     *         200:
+     *           description: "Shopping cart fetched successfully"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart not found for this user"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/username/{username}:
+     *     get:
+     *       summary: "Get shopping cart by username"
+     *       description: "Fetch a shopping cart by username."
+     *       operationId: "getShoppingCartByUsername"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "username"
+     *           in: "path"
+     *           description: "Username of the user"
+     *           required: true
+     *           type: "string"
+     *       responses:
+     *         200:
+     *           description: "Shopping cart fetched successfully"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart not found for this username"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/{cartId}/product/{productId}:
+     *     post:
+     *       summary: "Add a product to the shopping cart"
+     *       description: "Add a product to the shopping cart by product ID."
+     *       operationId: "addProductToCart"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "cartId"
+     *           in: "path"
+     *           description: "ID of the shopping cart"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *         - name: "productId"
+     *           in: "path"
+     *           description: "ID of the product"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *       responses:
+     *         200:
+     *           description: "Product added to the shopping cart"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart or product not found"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/{cartId}/product/{productId}/remove:
+     *     delete:
+     *       summary: "Remove a product from the shopping cart"
+     *       description: "Remove a product from the shopping cart by product ID."
+     *       operationId: "removeProductFromCart"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "cartId"
+     *           in: "path"
+     *           description: "ID of the shopping cart"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *         - name: "productId"
+     *           in: "path"
+     *           description: "ID of the product"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *       responses:
+     *         200:
+     *           description: "Product removed from the shopping cart"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart or product not found"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/{id}/clear:
+     *     delete:
+     *       summary: "Clear the shopping cart"
+     *       description: "Clear all products from the shopping cart."
+     *       operationId: "clearShoppingCart"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "id"
+     *           in: "path"
+     *           description: "ID of the shopping cart"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *       responses:
+     *         200:
+     *           description: "Shopping cart cleared successfully"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart not found"
+     *         500:
+     *           description: "Internal server error"
+     *   /shoppingcart/{cartId}/product/{productId}/quantity:
+     *     put:
+     *       summary: "Change the quantity of a product in the shopping cart"
+     *       description: "Update the quantity of a product in the shopping cart."
+     *       operationId: "changeProductQuantity"
+     *       tags:
+     *         - "Shopping Cart"
+     *       parameters:
+     *         - name: "cartId"
+     *           in: "path"
+     *           description: "ID of the shopping cart"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *         - name: "productId"
+     *           in: "path"
+     *           description: "ID of the product"
+     *           required: true
+     *           type: "integer"
+     *           format: "int64"
+     *         - name: "quantity"
+     *           in: "body"
+     *           description: "New quantity of the product"
+     *           required: true
+     *           schema:
+     *             type: "object"
+     *             properties:
+     *               quantity:
+     *                 type: "integer"
+     *       responses:
+     *         200:
+     *           description: "Product quantity updated"
+     *           schema:
+     *             $ref: "#/definitions/Shoppingcart"
+     *         404:
+     *           description: "Shopping cart or product not found"
+     *         400:
+     *           description: "Invalid quantity"
+     *         500:
+     *           description: "Internal server error"
+     * definitions:
+     *   Shoppingcart:
+     *     type: "object"
+     *     properties:
+     *       id:
+     *         type: "integer"
+     *         format: "int64"
+     *       products:
+     *         type: "array"
+     *         items:
+     *           $ref: "#/definitions/Product"
+     *       totalPrice:
+     *         type: "number"
+     *         format: "float"
+     *   Product:
+     *     type: "object"
+     *     properties:
+     *       id:
+     *         type: "integer"
+     *         format: "int64"
+     *       name:
+     *         type: "string"
+     *       price:
+     *         type: "number"
+     *         format: "float"
+     *       quantity:
+     *         type: "integer"
+     */
+    shoppingcartRouter.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const shoppingcart = await shoppingcartService.getShoppingCartByUsername({
-                username: req.params.username,
+            const shoppingcart = await shoppingcartService.getShoppingCartById({
+                id: parseInt(req.params.id),
             });
             res.status(200).json(shoppingcart);
         } catch (error) {
             next(error);
         }
-    }
-);
+    });
 
-shoppingcartRouter.post(
-    '/:cartId/product/:productId',
-    async (req: Request, res: Response, next: NextFunction) => {
+    shoppingcartRouter.get('/user/:userId', async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const shoppingcart = await shoppingcartService.addProductToCart(
-                parseInt(req.params.cartId),
-                parseInt(req.params.productId)
-            );
+            const shoppingcart = await shoppingcartService.getShoppingCartByUserId({
+                userId: parseInt(req.params.userId),
+            });
             res.status(200).json(shoppingcart);
         } catch (error) {
             next(error);
         }
-    }
-);
+    });
 
-shoppingcartRouter.delete(
-    '/:cartId/product/:productId',
-    async (req: Request, res: Response, next: NextFunction) => {
+    shoppingcartRouter.get(
+        '/username/:username',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const shoppingcart = await shoppingcartService.getShoppingCartByUsername({
+                    username: req.params.username,
+                });
+                res.status(200).json(shoppingcart);
+            } catch (error) {
+                next(error);
+            }
+        }
+    );
+
+    shoppingcartRouter.post(
+        '/:cartId/product/:productId',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const shoppingcart = await shoppingcartService.addProductToCart(
+                    parseInt(req.params.cartId),
+                    parseInt(req.params.productId)
+                );
+                res.status(200).json(shoppingcart);
+            } catch (error) {
+                next(error);
+            }
+        }
+    );
+
+    shoppingcartRouter.delete(
+        '/:cartId/product/:productId',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const shoppingcart = await shoppingcartService.removeProductFromCart(
+                    parseInt(req.params.cartId),
+                    parseInt(req.params.productId)
+                );
+                res.status(200).json(shoppingcart);
+            } catch (error) {
+                next(error);
+            }
+        }
+    );
+
+    shoppingcartRouter.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const shoppingcart = await shoppingcartService.removeProductFromCart(
-                parseInt(req.params.cartId),
-                parseInt(req.params.productId)
-            );
+            const shoppingcart = await shoppingcartService.clearShoppingCart({
+                id: parseInt(req.params.id),
+            });
             res.status(200).json(shoppingcart);
         } catch (error) {
             next(error);
         }
-    }
-);
+    });
 
-shoppingcartRouter.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const shoppingcart = await shoppingcartService.clearShoppingCart({
-            id: parseInt(req.params.id),
-        });
-        res.status(200).json(shoppingcart);
-    } catch (error) {
-        next(error);
-    }
-});
-
-shoppingcartRouter.put(
-    '/:cartId/product/:productId',
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const shoppingcart = await shoppingcartService.changeProductQuantity(
-                parseInt(req.params.cartId),
-                parseInt(req.params.productId),
-                parseInt(req.body.quantity)
-            );
-            res.status(200).json(shoppingcart);
-        } catch (error) {
-            next(error);
+    shoppingcartRouter.put(
+        '/:cartId/product/:productId',
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const shoppingcart = await shoppingcartService.changeProductQuantity(
+                    parseInt(req.params.cartId),
+                    parseInt(req.params.productId),
+                    parseInt(req.body.quantity)
+                );
+                res.status(200).json(shoppingcart);
+            } catch (error) {
+                next(error);
+            }
         }
-    }
-);
+    );
 
-export { shoppingcartRouter };
+    export { shoppingcartRouter };
--- back-end/controller/user.routes.ts
+++ back-end/controller/user.routes.ts
@@ -4,6 +4,174 @@ import userService from '../service/user.service';
 
 const userRouter = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Users
+ *     description: User management and authentication
+ * paths:
+ *   /signup:
+ *     post:
+ *       summary: Register a new user
+ *       description: Allows a user to sign up by providing their username, email, password, and role.
+ *       operationId: signup
+ *       tags:
+ *         - Users
+ *       requestBody:
+ *         description: User data required to create a new user.
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 username:
+ *                   type: string
+ *                   example: johndoe
+ *                 email:
+ *                   type: string
+ *                   example: johndoe@example.com
+ *                 password:
+ *                   type: string
+ *                   example: mysecurepassword
+ *                 role:
+ *                   type: string
+ *                   enum:
+ *                     - user
+ *                     - admin
+ *                   example: user
+ *               required:
+ *                 - username
+ *                 - email
+ *                 - password
+ *                 - role
+ *       responses:
+ *         '200':
+ *           description: User created successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         '400':
+ *           description: Invalid input data
+ *         '500':
+ *           description: Internal server error
+ *   /login:
+ *     post:
+ *       summary: User login
+ *       description: Authenticates a user with their username and password.
+ *       operationId: login
+ *       tags:
+ *         - Users
+ *       requestBody:
+ *         description: User credentials for login.
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 username:
+ *                   type: string
+ *                   example: johndoe
+ *                 password:
+ *                   type: string
+ *                   example: mysecurepassword
+ *               required:
+ *                 - username
+ *                 - password
+ *       responses:
+ *         '200':
+ *           description: Authentication successful
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   message:
+ *                     type: string
+ *                     example: Authentication successful
+ *                   token:
+ *                     type: string
+ *                     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMjM0NTY3ODkwLCJ1c2VybmFtZSI6Implbm5pZCIsImlhdCI6MTYzNzg5NDAwOX0.S9HhGfHewvq7_pd_o-73hQ_R0FvD1yP5duY_aDzPyH4'
+ *         '400':
+ *           description: Invalid username or password
+ *         '500':
+ *           description: Internal server error
+ *   /{id}:
+ *     get:
+ *       summary: Get user by ID
+ *       description: Fetches a user by their unique ID.
+ *       operationId: getUserById
+ *       tags:
+ *         - Users
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           required: true
+ *           description: ID of the user to retrieve
+ *           schema:
+ *             type: integer
+ *             example: 1
+ *       responses:
+ *         '200':
+ *           description: A user object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         '404':
+ *           description: User not found
+ *         '500':
+ *           description: Internal server error
+ *   /username/{username}:
+ *     get:
+ *       summary: Get user by username
+ *       description: Fetches a user by their username.
+ *       operationId: getUserByUsername
+ *       tags:
+ *         - Users
+ *       parameters:
+ *         - name: username
+ *           in: path
+ *           required: true
+ *           description: Username of the user to retrieve
+ *           schema:
+ *             type: string
+ *             example: johndoe
+ *       responses:
+ *         '200':
+ *           description: A user object
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         '404':
+ *           description: User not found
+ *         '500':
+ *           description: Internal server error
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         username:
+ *           type: string
+ *           example: johndoe
+ *         email:
+ *           type: string
+ *           example: johndoe@example.com
+ *         role:
+ *           type: string
+ *           enum:
+ *             - user
+ *             - admin
+ *           example: user
+ */
+
 userRouter.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
     try {
         const userInput = <UserInput>req.body;
@@ -18,7 +186,7 @@ userRouter.post('/login', async (req: Request, res: Response, next: NextFunction
     try {
         const userInput = <UserInput>req.body;
         const response = await userService.authenticate(userInput);
-        res.status(200).json({ message: 'Authentication succesful', ...response });
+        res.status(200).json({ message: 'Authentication successful', token: response.token });
     } catch (error) {
         next(error);
     }
